7e6b24cdbef9078786b7ed083eb66f6dfdb2e175
GangSc <gangqiu0@gmail.com>
2022-12-29 21:27:18 -0500

    adds basic model, adapter, and serializer

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,27 +1,33 @@
 import Route from '@ember/routing/route';
-
-const COMMUNITY_CATEGORIES = ['Condo', 'Townhouse', 'Apartment'];
+import { service } from '@ember/service';
 
 export default class IndexRoute extends Route {
+  // async model() {
+  //   let response = await fetch('/api/rentals.json');
+  //   let { data } = await response.json();
+  //
+  //   const dataa = data.map((model) => {
+  //     let { id, attributes } = model;
+  //     let type;
+  //
+  //     if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
+  //       type = 'Community';
+  //     } else {
+  //       type = 'Standalone';
+  //     }
+  //
+  //     return { id, type, ...attributes };
+  //   });
+  //
+  //   console.log('routes/index');
+  //   console.log(dataa);
+  //   return dataa;
+  // }
+
+  // code above is replaced with the model logic:
+  @service store;
+
   async model() {
-    let response = await fetch('/api/rentals.json');
-    let { data } = await response.json();
-
-    const dataa = data.map((model) => {
-      let { id, attributes } = model;
-      let type;
-
-      if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
-        type = 'Community';
-      } else {
-        type = 'Standalone';
-      }
-
-      return { id, type, ...attributes };
-    });
-
-    console.log('routes/index');
-    console.log(dataa);
-    return dataa;
+    return this.store.findAll('rental');
   }
 }
--- app/routes/rental.js
+++ app/routes/rental.js
@@ -1,23 +1,29 @@
 import Route from '@ember/routing/route';
-
-const COMMUNITY_CATEGORIES = ['Condo', 'Townhouse', 'Apartment'];
+import { service } from '@ember/service';
 
 export default class RentalRoute extends Route {
+  // async model(params) {
+  //   let response = await fetch(`/api/rentals/${params.rental_id}.json`);
+  //   let { data } = await response.json();
+  //
+  //   let { id, attributes } = data;
+  //   let type;
+  //   console.log('route/rentals');
+  //   console.log(data);
+  //
+  //   if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
+  //     type = 'Community';
+  //   } else {
+  //     type = 'Standalone';
+  //   }
+  //
+  //   return { id, type, ...attributes };
+  // }
+
+  // code above is unnecessary after using model logic:
+  @service store;
+
   async model(params) {
-    let response = await fetch(`/api/rentals/${params.rental_id}.json`);
-    let { data } = await response.json();
-
-    let { id, attributes } = data;
-    let type;
-    console.log('route/rentals');
-    console.log(data);
-
-    if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
-      type = 'Community';
-    } else {
-      type = 'Standalone';
-    }
-
-    return { id, type, ...attributes };
+    return this.store.findRecord('rental', params.rental_id);
   }
 }
